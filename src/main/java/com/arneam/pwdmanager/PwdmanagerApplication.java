687c30a8e342ffd8b6579035a24981312ff3d94b
Daniel Medeiros <daniel@arneam.com>
2019-10-18 08:47:18 -0300

    Add initial app files

---
+++ src/main/java/com/arneam/pwdmanager/PwdmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.arneam.pwdmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwdmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwdmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arneam/pwdmanager/PwdmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arneam.pwdmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwdmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
