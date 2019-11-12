a2e9e9dbead20986b772bfe35bde519a5571bf12
Daniel Medeiros <daniel@arneam.com>
2019-11-12 05:39:10 -0300

    Prepare CardPassword to be handled by JSON

--- src/main/java/com/arneam/pwdmanager/domain/CardPassword.java
+++ src/main/java/com/arneam/pwdmanager/domain/CardPassword.java
@@ -1,17 +1,34 @@
 package com.arneam.pwdmanager.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.experimental.Accessors;
+import org.springframework.data.redis.core.RedisHash;
 
 @Builder
 @Getter
 @Accessors(fluent = true)
-public class CardPassword implements Password {
+@RedisHash("CardPassword")
+@JsonDeserialize(builder = CardPassword.CardPasswordBuilder.class)
+public final class CardPassword implements Password {
 
+  @JsonProperty
+  @NonNull
   private String id;
+
+  @JsonProperty
+  @NonNull
   private String cardName;
+
+  @JsonProperty
+  @NonNull
   private String number;
+
+  @JsonProperty
+  @NonNull
   private String password;
 
 }
